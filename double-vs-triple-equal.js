3336d5899caca45e0c44a217029d75d395356a01
shamiul5vo <shamiulislam15@gmail.com>
2020-08-07 12:37:08 +0600

    double vs triple equal concept clear

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,12 @@
+// == number & string equal so true {only value check}
+// === (will check strictly) {value & type check korbe}
+
+
+const first = 0 ;
+const second = false ;
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
